88266d52cacab2b7ce3a0af73b8c99ca2cb2e98c
k-laughner <74153467+k-laughner@users.noreply.github.com>
2021-09-13 15:15:29 -0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,21 +5,24 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("What is the length of the room in feet?");
-        Scanner input = new Scanner(System.in);
-        int length = input.nextInt();
+  // constant
+  public class constant {
+   public static final double conv = 0.09290304;
+  }
+  public static void main(String[] args) {
+    System.out.println("What is the length of the room in feet?");
+    Scanner input = new Scanner(System.in);
+    int length = input.nextInt();
 
-        System.out.println("What is the width of the room in feet?");
-        Scanner input2 = new Scanner(System.in);
-        int width = input2.nextInt();
+    System.out.println("What is the width of the room in feet?");
+    Scanner input2 = new Scanner(System.in);
+    int width = input2.nextInt();
 
-        int feet = length*width;
-        double conversion = 0.09290304;
-        double meters = feet*conversion;
+    int feet = length*width;
+    double meters = feet*constant.conv;
 
-        System.out.println("You entered dimensions of " + length + " by " + width + " feet.");
+    System.out.println("You entered dimensions of " + length + " by " + width + " feet.");
 
-        System.out.println("The area is " + feet + " square feet or " + meters + " square meters.");
-    }
+    System.out.println("The area is " + feet + " square feet or " + meters + " square meters.");
+  }
 }
